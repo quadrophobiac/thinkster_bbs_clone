48405391473dfa46d1c4c970ccc034465cfc2d19
stephenfortune <stephen.j.fortune@gmail.com>
2014-12-22 20:06:48 +0000

    Posts view and controller added. Posts model contains an add and a delete method

---
+++ ang-news/app/scripts/controllers/posts.js
@@ -0,0 +1,18 @@
+/**
+ * Created by stephen on 22/12/14.
+ */
+'use strict';
+
+app.controller('PostsCtrl', function ($scope){
+  $scope.post = {url: 'http://', title: ''};
+  $scope.posts = [];
+
+  $scope.submitPost = function () {
+    $scope.posts.push($scope.post);
+    $scope.post = {url: 'http://', title: ''};
+  };
+  $scope.deletePost = function (index) {
+    $scope.posts.splice(index, 1);
+  };
+
+}); // end of controller
