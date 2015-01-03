0125778f2b85fc4d780034526694fd8f20b17a14
stephenfortune <stephen.j.fortune@gmail.com>
2015-01-03 15:45:48 +0000

    skeleton of all elements of the Auth object

--- ang-news/app/scripts/services/auth.js
+++ ang-news/app/scripts/services/auth.js
@@ -7,7 +7,14 @@ app.factory('Auth', function ($firebaseSimpleLogin, FIREBASE_URL, $rootScope){
   var ref;
   var auth;
 
-  var Auth = {};
+  var Auth = {
+    register,
+    login,
+    logout,
+    resolveUser,
+    signedIn,
+    user
+  };
 
   $rootScope.$on('', function(){});
 
