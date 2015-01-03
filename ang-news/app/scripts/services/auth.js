c9ad2042bb189efc769176139cbbf204ad4f6bc6
stephenfortune <stephen.j.fortune@gmail.com>
2015-01-03 15:43:44 +0000

    added skeleton of the Authentication service which avails of the Firebase module

---
+++ ang-news/app/scripts/services/auth.js
@@ -0,0 +1,15 @@
+/**
+ * Created by stephen on 03/01/15.
+ */
+'use strict';
+
+app.factory('Auth', function ($firebaseSimpleLogin, FIREBASE_URL, $rootScope){
+  var ref;
+  var auth;
+
+  var Auth = {};
+
+  $rootScope.$on('', function(){});
+
+  return Auth;
+});
