111573cdc7403a475267db615e8a7ab688838ba0
stephenfortune <stephen.j.fortune@gmail.com>
2015-01-03 15:51:29 +0000

    auth service populated with appropriate angular methods

--- ang-news/app/scripts/services/auth.js
+++ ang-news/app/scripts/services/auth.js
@@ -8,11 +8,21 @@ app.factory('Auth', function ($firebaseSimpleLogin, FIREBASE_URL, $rootScope){
   var auth;
 
   var Auth = {
-    register: function(user){},
-    login: function(user){},
-    logout: function(){},
-    resolveUser: function(){},
-    signedIn: function(){},
+    register: function(user){
+      return auth.$createUser(user.email, user.password);
+    },
+    login: function(user){
+      return auth.$login('password', user);
+    },
+    logout: function(){
+      auth.$logout();
+    },
+    resolveUser: function(){
+      return auth.$getCurrentUser();
+    },
+    signedIn: function(){
+      return !!Auth.user.provider;
+    },
     user: {}
   };
 
