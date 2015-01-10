b195aff2ec803d1a09ebc97f4bbc0b5c89932c49
stephenfortune <stephen.j.fortune@gmail.com>
2015-01-10 22:49:42 +0000

    /new posts associating functionality added to SubmitPost function presently located within the nav controller

--- ang-news/app/scripts/controllers/nav.js
+++ ang-news/app/scripts/controllers/nav.js
@@ -10,6 +10,10 @@ app.controller('NavCtrl', function($scope, $location, Post, Auth){
   $scope.user = Auth.user;
 
   $scope.submitPost = function (){
+    // new posts associating functionality
+    $scope.post.creator = $scope.user.profile.username;
+    $scope.post.creatorUID = $scope.user.uid;
+
     Post.create($scope.post).then(function(ref){
       $location.path('/posts/' + ref.name());
       $scope.post = {url: 'http://', title: ''};
