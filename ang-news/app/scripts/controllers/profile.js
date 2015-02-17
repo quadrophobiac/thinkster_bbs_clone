ff7ca358e22dc039c41a2c1fe5ecb0aab3b240c2
stephenfortune <stephen.j.fortune@gmail.com>
2015-02-17 20:25:12 +0000

    adding profiller controller

--- ang-news/app/scripts/controllers/profile.js
+++ ang-news/app/scripts/controllers/profile.js
@@ -3,27 +3,4 @@
  */
 'use strict';
 
-app.factory('Profile', function($window, FIREBASE_URL, $firebase, Post, $q){
-
-  var ref = new $window.Firebase(FIREBASE_URL);
-
-  var profile = {
-    get: function (userId) {
-      return $firebase(ref.child('profile').child(userId)).$asObject();
-    },
-    getPosts: function(userId) {
-      var defer = $q.defer();
-
-      $firebase()
-        .$asArray()
-        .$loaded()
-        .$then(function(data) {
-
-        });
-
-      return defer.promise;
-    }
-  };
-
-  return profile;
-});
+app.controller('ProfileCtrl', function(){});
---
+++ ang-news/app/scripts/services/profile.js
@@ -0,0 +1,37 @@
+/**
+ * Created by stephen on 17/02/15.
+ */
+'use strict';
+
+app.factory('Profile', function($window, FIREBASE_URL, $firebase, Post, $q){
+
+  var ref = new $window.Firebase(FIREBASE_URL);
+
+  var profile = {
+    get: function (userId) {
+      return $firebase(ref.child('profile').child(userId)).$asObject();
+    },
+    getPosts: function(userId) {
+      var defer = $q.defer();
+
+      $firebase(ref.child('user_posts').child(userId))
+        .$asArray()
+        .$loaded()
+        .$then(function(data) {
+          var posts = {};
+
+          for(var i=0; i<data.length; i++){
+            var value = data[i].$value;
+            posts[value] = Post.get(value);
+          }
+          defer.resolve(posts);
+        });
+
+      return defer.promise;
+      // Because we're making several queries to Firebase at once, we return a promise object that we then resolve with
+      // the post data.
+    }
+  };
+
+  return profile;
+});
