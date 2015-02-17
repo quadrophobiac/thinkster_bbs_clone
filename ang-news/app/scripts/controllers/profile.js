5247137ad4685274035c1de5dacb62c739a8d57d
stephenfortune <stephen.j.fortune@gmail.com>
2015-02-17 20:17:30 +0000

    profile service outline

--- ang-news/app/scripts/controllers/profile.js
+++ ang-news/app/scripts/controllers/profile.js
@@ -3,4 +3,18 @@
  */
 'use strict';
 
-app.factory('Profile', function(){});
+app.factory('Profile', function($window, FIREBASE_URL, $firebase, Post, $q){
+
+  var ref = new $window.Firebase(FIREBASE_URL);
+
+  var profile = {
+    get: function (userId) {
+      return ;
+    },
+    getPosts: function(userId) {
+
+    }
+  };
+
+  return profile;
+});
