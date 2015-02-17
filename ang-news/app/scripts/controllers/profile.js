9704a7e6358462e52a18353271c3ed6e39232fa6
stephenfortune <stephen.j.fortune@gmail.com>
2015-02-17 20:31:37 +0000

    example of new way of doing an ng-repeat here

--- ang-news/app/scripts/controllers/profile.js
+++ ang-news/app/scripts/controllers/profile.js
@@ -3,4 +3,12 @@
  */
 'use strict';
 
-app.controller('ProfileCtrl', function(){});
+app.controller('ProfileCtrl', function($scope, $routeParams, Profile){
+  var uid = $routeParams.userId;
+
+  $scope.profile = Profile.get(uid);
+  Profile.getPosts(uid).then(function(posts){
+    $scope.posts = posts;
+  });
+
+});
